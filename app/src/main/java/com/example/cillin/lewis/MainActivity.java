7cebf688a263169875157e576326d8d9328a96a7
CillinLyons <cillinlyons@outlook.com>
2015-07-27 13:37:01 +0100

    Inserting xml files

--- app/src/main/java/com/example/cillin/lewis/MainActivity.java
+++ app/src/main/java/com/example/cillin/lewis/MainActivity.java
@@ -1,38 +1,277 @@
 package com.example.cillin.lewis;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.sql.Timestamp;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements OnItemSelectedListener
+{
+    private Button btnAddNewCategory;
+    private TextView txtCategory;
+    public Spinner spinnerFood;
+    public TextView timeInput;
+
+    // array list for spinner adapter
+    private ArrayList<Location> categoriesList;
+    ProgressDialog pDialog;
+
+    // API urls
+    // Url to create new category
+    private String URL_NEW_CATEGORY = "http://192.168.1.4/food_api/new_category.php";
+    // Url to get all categories
+    private String URL_CATEGORIES = "http://192.168.1.4/food_api/get_categories.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddNewCategory = (Button) findViewById(R.id.btnAddNewCategory);
+        spinnerFood = (Spinner) findViewById(R.id.spinFood);
+        timeInput = (TextView) findViewById(R.id.textView2);
+
+        categoriesList = new ArrayList<Location>();
+
+        spinnerFood.setSelection(0, false);
+
+        // spinner item select listener
+        spinnerFood.setOnItemSelectedListener(this);
+
+        // Add new category click event
+        btnAddNewCategory.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (spinnerFood.getSelectedItem().toString().length() > 0) {
+
+                    /*java.util.Date dt = new java.util.Date();
+
+                    java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+                    String currentTime = sdf.format(dt);//current date and time here, '2015-07-17 18:38:46'
+
+                    Intent intent1 = new Intent(MainActivity.this, Map.class);
+                    intent1.putExtra("DATE_KEY", currentTime);
+                    startActivity(intent1);*/
+
+                    //intent1.putExtra("SPINNER_KEY", newCategory);
+                    // Call Async task to create new category
+                    new AddNewCategory().execute();
+                    Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                    startActivity(intent);
+
+                    //setSpinnerSelectionWithoutCallingListener(spinnerFood, null);
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter category name", Toast.LENGTH_SHORT)
+                            .show();
+                }
+
+            }
+        });
+
+        new GetCategories().execute();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**
+     * Adding spinner data
+     * */
+    public void populateSpinner() {
+        List<String> lables = new ArrayList<String>();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for (int i = 0; i < categoriesList.size(); i++) {
+            lables.add(categoriesList.get(i).getName());
         }
 
-        return super.onOptionsItemSelected(item);
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, lables);
+
+        // Drop down layout style - list view with radio button
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinnerFood.setAdapter(spinnerAdapter);
+
+    }
+
+    /**
+     * Async task to get all food categories
+     * */
+    private class GetCategories extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Fetching locations..");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            ServiceHandler jsonParser = new ServiceHandler();
+            String json = jsonParser.makeServiceCall(URL_CATEGORIES, ServiceHandler.GET);
+
+            Log.e("Response: ", "> " + json);
+
+            if (json != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(json);
+                    if (jsonObj != null) {
+                        JSONArray green_line = jsonObj
+                                .getJSONArray("green_line");
+
+                        for (int i = 0; i < green_line.length(); i++) {
+                            JSONObject catObj = (JSONObject) green_line.get(i);
+                            Location cat = new Location(catObj.getInt("id"),
+
+
+                                    catObj.getString("name"));
+                            categoriesList.add(cat);
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                Log.e("JSON Data", "Didn't receive any data from server!");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            populateSpinner();
+        }
+    }
+
+    /**
+     * Async task to create a new food category
+     * */
+    private class AddNewCategory extends AsyncTask<String, Void, Void> {
+
+        boolean isNewCategoryCreated = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Updating location..");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(String... args) {
+
+            String newCategory = spinnerFood.getSelectedItem().toString();
+
+            // Preparing post params
+            List<NameValuePair> paramsa = new ArrayList<NameValuePair>();
+            paramsa.add(new BasicNameValuePair("name", newCategory));
+            //params.add(new BasicNameValuePair("dtime", currentTime));
+
+            ServiceHandler serviceClient = new ServiceHandler();
+
+            String json = serviceClient.makeServiceCall(URL_NEW_CATEGORY,
+                    ServiceHandler.POST, paramsa);
+
+            Log.d("Create Response: ", "> " + json);
+
+            if (json != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(json);
+                    boolean error = jsonObj.getBoolean("error");
+                    // checking for error node in json
+                    if (!error) {
+                        // new category created successfully
+                        isNewCategoryCreated = true;
+                    } else {
+                        Log.e("Create Category Error: ", "> " + jsonObj.getString("message"));
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                Log.e("JSON Data", "Didn't receive any data from server!");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            populateSpinner();
+            if (isNewCategoryCreated) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // fetching all categories
+                        new GetCategories().execute();
+                    }
+                });
+            }
+        }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+
+        Toast.makeText(
+                getApplicationContext(),
+                parent.getItemAtPosition(position).toString() + " Selected" ,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
     }
 }
--- app/src/main/java/com/example/cillin/lewis/RecentStops.java
+++ app/src/main/java/com/example/cillin/lewis/RecentStops.java
@@ -1,7 +1,166 @@
 package com.example.cillin.lewis;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
 /**
  * Created by cillin on 27/07/2015.
  */
-public class RecentStops {
+public class RecentStops extends ListActivity
+{
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> productsList;
+
+    // url to get all products list
+    private static String url_all_products = "http://192.168.1.4/food_api/get_all_stops.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_LEWIS_LOCATIONS = "lewis_locations";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_DTIME = "dtime";
+
+
+    // products JSONArray
+    JSONArray lewis_locations = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recentstops);
+
+        // Hashmap for ListView
+        productsList = new ArrayList<HashMap<String, String>>();
+
+        // Loading products in Background Thread
+        new LoadAllProducts().execute();
+
+        // Get listview
+        ListView lv = getListView();
+
+    }
+
+    /**
+     * Background Async Task to Load all product by making HTTP Request
+     * */
+    class LoadAllProducts extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(RecentStops.this);
+            pDialog.setMessage("Loading locations. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    lewis_locations = json.getJSONArray(TAG_LEWIS_LOCATIONS);
+
+                    // looping through All Products
+                    for (int i = 0; i < lewis_locations.length(); i++) {
+                        JSONObject c = lewis_locations.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String name = c.getString(TAG_NAME);
+                        String dtime = c.getString(TAG_DTIME);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_NAME, name);
+                        map.put(TAG_DTIME, dtime);
+
+                        // adding HashList to ArrayList
+                        productsList.add(map);
+                    }
+                } else {
+                    // no products found
+                    // Launch Add New product Activity
+                    Intent i = new Intent(getApplicationContext(),
+                            MainActivity.class);
+                    // Closing all previous activities
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    /**
+                     * Updating parsed JSON data into ListView
+                     * */
+                    ListAdapter adapter = new SimpleAdapter(
+                            RecentStops.this, productsList,
+                            R.layout.list_item, new String[] {
+                            TAG_NAME, TAG_DTIME},
+                            new int[] { R.id.stop, R.id.dtime });
+                    // updating listview
+                    setListAdapter(adapter);
+                }
+            });
+
+        }
+
+    }
 }
